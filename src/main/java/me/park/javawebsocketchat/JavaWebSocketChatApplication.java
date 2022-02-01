1250e5af5eef66ec302aa0ad4e34b68ba7160089
Park SungHwan <park0219ok@outlook.com>
2022-02-01 20:36:40 +0900

    Initial commit

---
+++ src/main/java/me/park/javawebsocketchat/JavaWebSocketChatApplication.java
@@ -0,0 +1,13 @@
+package me.park.javawebsocketchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebSocketChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebSocketChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/park/javawebsocketchat/JavaWebSocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package me.park.javawebsocketchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebSocketChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
