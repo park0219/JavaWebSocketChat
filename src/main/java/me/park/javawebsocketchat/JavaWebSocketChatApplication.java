44fd35691b74b33a8da3a79285b23929282efde6
Park SungHwan <park0219ok@outlook.com>
2022-02-05 02:33:57 +0900

    chore: spring-boot-starter-websocket 추가

--- src/main/java/me/park/javawebsocketchat/JavaWebSocketChatApplication.java
+++ src/main/java/me/park/javawebsocketchat/JavaWebSocketChatApplication.java
@@ -2,12 +2,21 @@ package me.park.javawebsocketchat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
+@EnableWebSocket
 public class JavaWebSocketChatApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaWebSocketChatApplication.class, args);
+    }
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
 
 }
