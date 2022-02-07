167b6585ea081ffcc90d6e49d5ee8242c22becef
Park SungHwan <park0219ok@outlook.com>
2022-02-07 10:24:39 +0900

    feat: message 출력 기능 추가

---
+++ src/main/java/me/park/javawebsocketchat/Message.java
@@ -0,0 +1,19 @@
+package me.park.javawebsocketchat;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Message {
+
+    String message = "";
+    String link = "";
+
+    public Message(String message, String link) {
+        this.message = message;
+        this.link = link;
+    }
+}
