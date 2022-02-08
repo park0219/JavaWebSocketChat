6a4913d76b7df96a84fe4fbfabc89e9842de49a8
Park SungHwan <park0219ok@outlook.com>
2022-02-08 13:15:50 +0900

    feat: lucy-xss-servlet 추가

--- src/main/java/me/park/javawebsocketchat/JavaWebSocketChatApplication.java
+++ src/main/java/me/park/javawebsocketchat/JavaWebSocketChatApplication.java
@@ -1,7 +1,9 @@
 package me.park.javawebsocketchat;
 
+import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
@@ -17,6 +19,15 @@ public class JavaWebSocketChatApplication {
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
+    }
+
+    @Bean
+    public FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean() {
+        FilterRegistrationBean<XssEscapeServletFilter> filterRegistration = new FilterRegistrationBean<>();
+        filterRegistration.setFilter(new XssEscapeServletFilter());
+        filterRegistration.setOrder(1);
+        filterRegistration.addUrlPatterns("/*");
+        return filterRegistration;
     }
 
 }
