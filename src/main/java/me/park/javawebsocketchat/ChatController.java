25e749514ca391c9852506fae6afcb8609a03460
Park SungHwan <park0219ok@outlook.com>
2022-02-14 01:52:09 +0900

    feat: 사용자가 채팅에 접속할 때 5분 전 메시지까지 불러오는 기능 추가

--- src/main/java/me/park/javawebsocketchat/ChatController.java
+++ src/main/java/me/park/javawebsocketchat/ChatController.java
@@ -1,5 +1,6 @@
 package me.park.javawebsocketchat;
 
+import com.google.gson.Gson;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 public class ChatController {
+
+    private final ChatRepository chatRepository;
+    private final Gson gson = new Gson();
+
+    public ChatController(ChatRepository chatRepository) {
+        this.chatRepository = chatRepository;
+    }
 
     @RequestMapping(value = "/chat.do", method = RequestMethod.GET)
     public String chat(HttpServletRequest request, HttpServletResponse response, Model model) {
@@ -20,6 +30,13 @@ public class ChatController {
             model.addAttribute("message", new Message("정상적인 경로로 접근해주세요.", "/logout.do"));
             return "message";
         }
+
+        //5분 전 대화까지 검색
+        LocalDateTime startDate = LocalDateTime.now().minusMinutes(5);
+        LocalDateTime endDate = LocalDateTime.now();
+        List<ChatEntity> chatList = chatRepository.findByChatRegdateBetweenOrderByChatRegdateAsc(startDate, endDate);
+
+        model.addAttribute("chatList", gson.toJson(chatList));
 
         return "chat";
     }
--- src/main/java/me/park/javawebsocketchat/ChatRepository.java
+++ src/main/java/me/park/javawebsocketchat/ChatRepository.java
@@ -3,6 +3,11 @@ package me.park.javawebsocketchat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface ChatRepository extends JpaRepository<ChatEntity, Long> {
+
+    List<ChatEntity> findByChatRegdateBetweenOrderByChatRegdateAsc(LocalDateTime start, LocalDateTime end);
 }
