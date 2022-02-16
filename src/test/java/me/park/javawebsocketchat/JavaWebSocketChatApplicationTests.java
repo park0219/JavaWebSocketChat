1e35c4d5a2e05a864421b1df34a14883681b9451
Park SungHwan <park0219ok@outlook.com>
2022-02-16 20:51:28 +0900

    test: 테스트 코드에 조건 추가

--- src/test/java/me/park/javawebsocketchat/JavaWebSocketChatApplicationTests.java
+++ src/test/java/me/park/javawebsocketchat/JavaWebSocketChatApplicationTests.java
@@ -3,7 +3,7 @@ package me.park.javawebsocketchat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class JavaWebSocketChatApplicationTests {
 
     @Test
